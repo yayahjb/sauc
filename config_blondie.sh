bee02f81e31f4103de6982f20f697c5f667f7225
Herbert J. Bernstein <yayahjb@gmail.com>
2016-11-27 13:19:34 +0000

    Update blondie configuration. -- HJB

--- config_blondie.sh
+++ config_blondie.sh
@@ -1,12 +1,11 @@
 #!/bin/sh
 HTTPDSERVER=http://blondie.ddns.net:8083;export HTTPDSERVER
-HTDOCS=/var/www/sauc-0.9.0;export HTDOCS;
-SEARCHURL=http://blondie.ddns.net:8083/sauc-0.9.0/;export SEARCHURL;
+HTDOCS=/var/www/sauc-1.0.0;export HTDOCS;
+SEARCHURL=http://blondie.ddns.net:8083/sauc-1.0.0/;export SEARCHURL;
 CGIPATH=http://blondie.ddns.net:8083/cgi-bin;export CGIPATH;
 CGIBIN=/var/www/cgi-bin;export CGIBIN;
 CGIMETHOD=GET;export CGIMETHOD;
 BINDEST=/var/www/cgi-bin;export BINDEST;
 PDBCELLINDEXURL=http://www.bmrb.wisc.edu/ftp/pub/pdb/derived_data/index/crystal.idx;export PDBCELLINDEXURL;
 PDBENTRIESURL=http://www.bmrb.wisc.edu/ftp/pub/pdb/derived_data/index/entries.idx;export PDBENTRIESURL;
-export CXXFLAGS="-Wall -g -O3 -DUSE_LOCAL_HEADERS -g -I. -I/usr/local/include -L/usr/local/lib"
-export LD_LIBRARY_PATH="/usr/local/lib";
+export CXXFLAGS="-Wall -O3 -DUSE_LOCAL_HEADERS -g -fopenmp  -ftree-parallelize-loops=8 -mfpmath=sse -msse2 -I.";
