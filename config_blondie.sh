6e6a224cb7a709c11f2cab17344e6fe0651b5a29
yayahjb <yayahjb@gmail.com>
2017-12-29 18:03:40 -0500

    Update for blondie install -- HJB

--- config_blondie.sh
+++ config_blondie.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
-HTTPDSERVER=http://blondie.ddns.net:8083;export HTTPDSERVER
-HTDOCS=/var/www/sauc-1.0.0;export HTDOCS;
-SEARCHURL=http://blondie.ddns.net:8083/sauc-1.0.0/;export SEARCHURL;
-CGIPATH=http://blondie.ddns.net:8083/cgi-bin;export CGIPATH;
+HTTPDSERVER=http://blondie.arcib.org:8083;export HTTPDSERVER
+HTDOCS=/var/www/sauc-1.0.2;export HTDOCS;
+SEARCHURL=http://blondie.arcib.org:8083/sauc-1.0.2/;export SEARCHURL;
+CGIPATH=http://blondie.arcib.org:8083/cgi-bin;export CGIPATH;
 CGIBIN=/var/www/cgi-bin;export CGIBIN;
 CGIMETHOD=GET;export CGIMETHOD;
 BINDEST=/var/www/cgi-bin;export BINDEST;
-PDBCELLINDEXURL=http://www.bmrb.wisc.edu/ftp/pub/pdb/derived_data/index/crystal.idx;export PDBCELLINDEXURL;
-PDBENTRIESURL=http://www.bmrb.wisc.edu/ftp/pub/pdb/derived_data/index/entries.idx;export PDBENTRIESURL;
+PDBCELLINDEXURL=http://ftp.wwpdb.org/pub/pdb/derived_data/index/crystal.idx;export PDBCELLINDEXURL;
+PDBENTRIESURL=http://ftp.wwpdb.org/pub/pdb/derived_data/index/entries.idx;export PDBENTRIESURL;
 export CXXFLAGS="-Wall -O3 -DUSE_LOCAL_HEADERS -g -fopenmp  -ftree-parallelize-loops=8 -mfpmath=sse -msse2 -I.";
