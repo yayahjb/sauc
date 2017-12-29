60175345d502fd04bfce325f71b662a1d7cf9fe2
Herbert J. Bernstein <yayahjb@gmail.com>
2017-12-29 18:19:10 -0500

    clean up configuration scripts and put cif directory under git control -- HJB

--- config_sf.sh
+++ config_sf.sh
@@ -1,11 +1,13 @@
 #!/bin/sh
 HTTPDSERVER=iterate.sf.net;export HTTPDSERVER
-HTDOCS=/home/project-web/iterate/htdocs/sauc-1.0.0;export HTDOCS;
-SEARCHURL=http://iterate.sf.net/sauc-1.0.0;export SEARCHURL;
+HTDOCS=/home/project-web/iterate/htdocs/sauc-1.0.2;export HTDOCS;
+SEARCHURL=http://iterate.sf.net/sauc-1.0.2;export SEARCHURL;
 CGIPATH=http://iterate.sf.net/cgi-bin;export CGIPATH;
 CGIBIN=/home/project-web/iterate/cgi-bin;export CGIBIN;
 CGIMETHOD=GET;export CGIMETHOD;
 BINDEST=/home/project-web/iterate/cgi-bin;export BINDEST;
-PDBCELLINDEXURL=http://www.bmrb.wisc.edu/ftp/pub/pdb/derived_data/index/crystal.idx;export PDBCELLINDEXURL;
-PDBENTRIESURL=http://www.bmrb.wisc.edu/ftp/pub/pdb/derived_data/index/entries.idx;export PDBENTRIESURL;
+PDBCELLINDEXURL=http://ftp.wwpdb.org/pub/pdb/derived_data/index/crystal.idx;export PDBCELLINDEXURL;
+PDBENTRIESURL=http://ftp.wwpdb.org/pub/pdb/derived_data/index/entries.idx;export PDBENTRIESURL;
+#PDBCELLINDEXURL=http://www.bmrb.wisc.edu/ftp/pub/pdb/derived_data/index/crystal.idx;export PDBCELLINDEXURL;
+#PDBENTRIESURL=http://www.bmrb.wisc.edu/ftp/pub/pdb/derived_data/index/entries.idx;export PDBENTRIESURL;
 export CXXFLAGS="-Wall -O3 -DUSE_LOCAL_HEADERS -g -I$HOME/include"
