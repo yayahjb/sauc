d888adb1a0d2f366d4cec720537c39783a0d5ea9
Herbert J. Bernstein <yayahjb@gmail.com>
2017-12-29 18:19:10 -0500

    clean up configuration scripts and put cif directory under git control -- HJB

--- config_cgiwrapped_user.sh
+++ config_cgiwrapped_user.sh
@@ -2,6 +2,8 @@
 HTTPDSERVER=`hostname -f`/~${USER};export HTTPDSERVER
 SEARCHURL=http://`hostname -f`/~${USER}/sauc;export SEARCHURL
 CGIPATH=http://`hostname -f`/cgi-bin/cgiwrap/${USER};export CGIPATH
-PDBCELLINDEXURL=http://www.bmrb.wisc.edu/ftp/pub/pdb/derived_data/index/crystal.idx;export PDBCELLINDEXURL;
-PDBENTRIESURL=http://www.bmrb.wisc.edu/ftp/pub/pdb/derived_data/index/entries.idx;export PDBENTRIESURL;
+PDBCELLINDEXURL=http://ftp.wwpdb.org/pub/pdb/derived_data/index/crystal.idx;export PDBCELLINDEXURL;
+PDBENTRIESURL=http://ftp.wwpdb.org/pub/pdb/derived_data/index/entries.idx;export PDBENTRIESURL;
+#PDBCELLINDEXURL=http://www.bmrb.wisc.edu/ftp/pub/pdb/derived_data/index/crystal.idx;export PDBCELLINDEXURL;
+#PDBENTRIESURL=http://www.bmrb.wisc.edu/ftp/pub/pdb/derived_data/index/entries.idx;export PDBENTRIESURL;
 CGIMETHOD=GET;export CGIMETHOD;
